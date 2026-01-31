bfc548961bbad1408f88cfbb348d4d6803cdf72a
yawarKhaplu <yawarsdocument@gmail.com>
2026-02-01 03:17:53 +0500

    adding something

---
+++ test/test_model.py
@@ -0,0 +1,18 @@
+import torch
+from tokenizer.tokenizer import CharTokenizer
+from model.minigpt import MiniGPT
+
+tokenizer = CharTokenizer()
+
+model = MiniGPT(
+    vocab_size=tokenizer.vocab_size,
+    block_size=32,
+)
+
+# fake input (batch=1, sequence=10)
+idx = torch.randint(0, tokenizer.vocab_size, (1, 10))
+
+logits, loss = model(idx)
+
+print("Logits shape:", logits.shape)
+print("Loss:", loss)
